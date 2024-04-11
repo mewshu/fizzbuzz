011d238b5f99a9e7b88fc092cb2003f90046df77
mewshu <51234575+mewshu@users.noreply.github.com>
2024-04-11 09:40:52 -0500

    got it first try! confidence through the roof.

---
+++ index.js
@@ -0,0 +1,20 @@
+/* 
+prompt user for number 
+if the number is % 3 == 0 return Fizz
+else if the number is % 5 == 0 return Buzz
+else if the number is % 3 && # 5 == 0 return FizzBuzz
+*/
+
+let FizzBuzz = (num) => {
+    for (i=1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else console.log(i);
+    }
+}
+
+console.log(FizzBuzz(30));
